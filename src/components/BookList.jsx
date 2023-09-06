6cd6e04ba8e5801d31be0cdecf6f492395a20302
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-09-06 16:01:49 +0200

    refactoring

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -6,25 +6,22 @@ class BookList extends Component {
   state = {
     filterString: "",
   };
-  filterBookList = string => {
-    this.setState({ filterString: string });
-  };
+  filterBookList = books =>
+    books.filter(book => book.title.toLowerCase().includes(this.state.filterString.toLowerCase()));
   render() {
     return (
       <>
         <InputGroup className="mb-3">
-          <Form.Control placeholder="Cerca.." onChange={e => this.filterBookList(e.target.value)} />
+          <Form.Control placeholder="Cerca.." onChange={e => this.setState({ filterString: e.target.value })} />
         </InputGroup>
         <Row className="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 gy-4 mb-5">
-          {this.props.books
-            .filter(book => book.title.toLowerCase().includes(this.state.filterString.toLowerCase()))
-            .map(book => {
-              return (
-                <Col key={book.asin} className="d-flex">
-                  <SingleBook book={book} />
-                </Col>
-              );
-            })}
+          {this.filterBookList(this.props.books).map(book => {
+            return (
+              <Col key={book.asin} className="d-flex">
+                <SingleBook book={book} />
+              </Col>
+            );
+          })}
         </Row>
       </>
     );
