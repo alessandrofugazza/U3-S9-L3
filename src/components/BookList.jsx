98c947b12d939f9d7e5a306e1e7fade9b7181ab9
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-09-06 19:15:16 +0200

    changes placeholder

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -12,7 +12,10 @@ class BookList extends Component {
     return (
       <>
         <InputGroup className="mb-3">
-          <Form.Control placeholder="Cerca.." onChange={e => this.setState({ filterString: e.target.value })} />
+          <Form.Control
+            placeholder="Search a title.."
+            onChange={e => this.setState({ filterString: e.target.value })}
+          />
         </InputGroup>
         <Row className="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 gy-4 mb-5">
           {this.filterBookList(this.props.books).map(book => {
