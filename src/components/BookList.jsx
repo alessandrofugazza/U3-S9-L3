50ac21b1a19efd05f0362ceb1e9499989cb76a7d
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-09-07 09:43:23 +0200

    refactor

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -6,14 +6,18 @@ class BookList extends Component {
   state = {
     filterString: "",
   };
-  handleChange = e => this.setState({ filterString: e.target.value });
+  handleChange = string => this.setState({ filterString: string });
   filterBookList = books =>
     books.filter(book => book.title.toLowerCase().includes(this.state.filterString.toLowerCase()));
   render() {
     return (
       <>
         <InputGroup className="mb-3">
-          <Form.Control placeholder="Search a title.." value={this.state.filterString} onChange={this.handleChange} />
+          <Form.Control
+            placeholder="Search a title.."
+            value={this.state.filterString}
+            onChange={e => this.handleChange(e.target.value)}
+          />
         </InputGroup>
         <Row className="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 gy-4 mb-5">
           {this.filterBookList(this.props.books).map(book => {
